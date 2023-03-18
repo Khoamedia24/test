5367336b37d20b6dd26ea1c76e3de8e1860803b6
Khoamedia24 <88242520+Khoamedia24@users.noreply.github.com>
2023-03-18 22:14:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Front-end Developer","Youtuber","Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    BackDelay: 1000,
+    loop: true
+})
